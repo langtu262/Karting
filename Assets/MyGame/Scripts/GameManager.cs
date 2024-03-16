f14a116e0db35f17c7b8391893e202591dcdc935
langtu262 <trungkienit87@gmail.com>
2024-03-16 22:10:55 +0700

    1

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int positionCar;
+    public int labsCar;
+    public int coinCar;
+    public float km;
+    private static GameManager _instace;
+    public static GameManager Instace
+    {
+        get { return _instace; }
+        set { _instace = value; }
+    }
+
+    void Start()
+    {
+        _instace = this;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetCoinCar()
+    {
+        coinCar += 5;
+    }
+}
---
+++ Assets/MyGame/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI; //thư viện cho hàm Text
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI m_TextPosition;
+    [SerializeField] private TextMeshProUGUI m_TextLaps;
+    [SerializeField] private TextMeshProUGUI m_TextCoin;
+    [SerializeField] private TextMeshProUGUI m_TextKm;
+
+    //[SerializeField] private Text MyText; // phai su dung lagecy Text
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_TextPosition.text = GameManager.Instace.positionCar.ToString();
+        m_TextLaps.text = GameManager.Instace.labsCar.ToString();
+        m_TextCoin.text = GameManager.Instace.coinCar.ToString();
+        m_TextKm.text = GameManager.Instace.km.ToString();
+    }
+}
